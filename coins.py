2fdc3a4480363236d16e3a1a3f051857800a012c
nnnLik <makhmudov_ra@vk.com>
2022-09-11 13:02:55 +0300

    fix dict struct

--- coins.py
+++ coins.py
@@ -1,4 +1,5 @@
-coins = {
+all_coins = {
+
     'BTC': ['BTC - Bitcoin', 'https://api.coingecko.com/api/v3/coins/bitcoin'],
     'ETH': ['ETH - Ethereum', 'https://api.coingecko.com/api/v3/coins/ethereum'],
     'TUSD': ['TUSD - TrueUSD', 'https://api.coingecko.com/api/v3/coins/true-usd'],
