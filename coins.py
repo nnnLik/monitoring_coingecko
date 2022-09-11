5a7e6ad52f68b1124d6016d01a2d81995f337a89
nnnLik <makhmudov_ra@vk.com>
2022-09-11 14:52:36 +0300

    rename key of coins

--- coins.py
+++ coins.py
@@ -1,8 +1,8 @@
 all_coins = {
 
-    'BTC': ['BTC - Bitcoin', 'https://api.coingecko.com/api/v3/coins/bitcoin'],
-    'ETH': ['ETH - Ethereum', 'https://api.coingecko.com/api/v3/coins/ethereum'],
-    'TUSD': ['TUSD - TrueUSD', 'https://api.coingecko.com/api/v3/coins/true-usd'],
+    1: ['BTC - Bitcoin', 'https://api.coingecko.com/api/v3/coins/bitcoin'],
+    2: ['ETH - Ethereum', 'https://api.coingecko.com/api/v3/coins/ethereum'],
+    3: ['TUSD - TrueUSD', 'https://api.coingecko.com/api/v3/coins/true-usd'],
 
 
 }
