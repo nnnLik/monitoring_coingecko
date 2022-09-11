008a3917f6303c54cd0399313b2e962e3d933386
nnnLik <makhmudov_ra@vk.com>
2022-09-11 09:54:07 +0300

    feat coin dict with req

---
+++ coins.py
@@ -0,0 +1,7 @@
+coins = {
+    'BTC': 'https://api.coingecko.com/api/v3/coins/bitcoin',
+    'ETH': 'https://api.coingecko.com/api/v3/coins/ethereum',
+    'TUSD': 'https://api.coingecko.com/api/v3/coins/true-usd',
+
+
+}
