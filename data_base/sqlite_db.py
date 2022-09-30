5b718c3079514190e4b16d43e8908448931f42cf
nnnLik <makhmudov_ra@vk.com>
2022-09-30 12:57:06 +0300

    feat сheck user_id and charging test money

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -9,3 +9,14 @@ def sql_start():
         print('Data base connected OK!')
     base.execute('CREATE TABLE IF NOT EXISTS wallet(user_id INTEGER PRIMARY KEY, balance FLOAT)')
     base.commit()
+
+
+async def check_user(user_id):
+    info = cur.execute('SELECT user_id FROM wallet WHERE user_id=?', (user_id,))
+
+    if info.fetchone() is None:
+        cur.execute('INSERT INTO wallet VALUES (?, ?)', (user_id, 1000))
+    else:
+        raise ValueError
+
+
