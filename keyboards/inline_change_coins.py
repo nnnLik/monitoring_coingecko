6af32e90ce1fb6b2ec590de0c0cd10030afab226
nnnLik <makhmudov_ra@vk.com>
2022-09-30 20:33:47 +0300

    style: rename inline keyboards, style: rename callback data, feat callback create wallet

--- keyboards/inline_change_coins.py
+++ keyboards/inline_change_coins.py
@@ -1,7 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-inline_keyboard_ctc = InlineKeyboardMarkup(row_width=2)
-inline_keyboard_ctc_buttons = [InlineKeyboardButton(text='🟢 ADD', callback_data='ctc_add'),
-                               InlineKeyboardButton(text='🔴 DELETE', callback_data='ctc_delete')]
-inline_keyboard_ctc.row(*inline_keyboard_ctc_buttons)
+inline_keyboard_CTC = InlineKeyboardMarkup(row_width=2)
+inline_keyboard_CTC_buttons = [InlineKeyboardButton(text='🟢 ADD', callback_data='CTC_add'),
+                               InlineKeyboardButton(text='🔴 DELETE', callback_data='CTC_delete'), ]
+inline_keyboard_CTC.row(*inline_keyboard_CTC_buttons)
--- keyboards/inline_check_price.py
+++ keyboards/inline_check_price.py
@@ -1,8 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
-inline_keyboard_cp = InlineKeyboardMarkup(row_width=2)
-inline_keyboard_cp_buttons = [InlineKeyboardButton(text='＄ - USD', callback_data='cp_usd'),
-                              InlineKeyboardButton(text='€ - EUR', callback_data='cp_eur'),
-                              InlineKeyboardButton(text='₽ - RUB', callback_data='cp_rub')]
-inline_keyboard_cp.row(*inline_keyboard_cp_buttons)
+inline_keyboard_CP = InlineKeyboardMarkup(row_width=2)
+inline_keyboard_CP_buttons = [InlineKeyboardButton(text='＄ - USD', callback_data='CP_usd'),
+                              InlineKeyboardButton(text='€ - EUR', callback_data='CP_eur'),
+                              InlineKeyboardButton(text='₽ - RUB', callback_data='CP_rub'), ]
+inline_keyboard_CP.row(*inline_keyboard_CP_buttons)
