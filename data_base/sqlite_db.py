df4ad17bb3bde59260ce37a26ca88d8cde87ce77
nnnLik <makhmudov_ra@vk.com>
2022-09-30 11:40:53 +0300

    feat basic wallet database

--- data_base/sqlite_db.py
+++ data_base/sqlite_db.py
@@ -1,9 +1,11 @@
-import sqlite3 as sq
+import sqlite3
+
 
 def sql_start():
     global base, cur
-    base = sq.connect('wallet_db')
+    base = sqlite3.connect('wallet_db')
     cur = base.cursor()
     if base:
         print('Data base connected OK!')
-    # base.execute('CREATE TABLE IF NOT EXISTS ')
+    base.execute('CREATE TABLE IF NOT EXISTS wallet(user_id INTEGER PRIMARY KEY, balance FLOAT)')
+    base.commit()
