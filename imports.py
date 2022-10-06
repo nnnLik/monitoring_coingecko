b5adedd7662710128312ba95ab305cca709d5962
nnnLik <makhmudov_ra@vk.com>
2022-10-06 20:20:10 +0300

    feat: module mediator

---
+++ imports.py
@@ -0,0 +1,14 @@
+import logging
+
+from aiogram import (Bot,
+                     Dispatcher,
+                     )
+
+from config import settings
+
+logging.basicConfig(level=logging.INFO)
+
+basic_message = 'Choose the functionality you are interested in'
+
+bot = Bot(token=settings['TOKEN'])
+dp = Dispatcher(bot=bot)
